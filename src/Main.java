d51baf6f60b932b8732781ca3f47e238a4a21c0f
LauraC <ldcaperav@gmail.com>
2025-08-28 13:00:41 -0500

    Comandos nuevos

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,12 @@ public class Main {
         System.out.println("1. git status /Estado rama ");
         System.out.println("2. git add . /agrupar archivos ");
         System.out.println("3. git commit -m /guardar en local ");
-        System.out.println("4. git push origin main /subir al reositorio ");
+        System.out.println("4. git push origin (en comillas en nombre de la rama) /subir al reositorio ");
         System.out.println("5. git clone /clonar ");
         System.out.println("6. git branch /ramas creadas");
         System.out.println("7. git fetch -v 7ramas remotas");
         System.out.println("8. git switch -c /crear rama y posicionarnos  ");
+        System.out.println("9. git checkout (en comillas en nombre de la rama) /cambiar de rama");
+        System.out.println("10. git pull origin (en comillas en nombre de la rama) /cambios de remoto a local");
         }
     }
