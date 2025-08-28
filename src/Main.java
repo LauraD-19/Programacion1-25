d8ce2fafdc5f8dfffec8b7c29cacbba667773bf4
LauraC <ldcaperav@gmail.com>
2025-08-28 12:40:15 -0500

    Cambios 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola");
+
+        System.out.println("aprendiendo en git");
+        System.out.println("comandos: ");
+        System.out.println("1. git status /Estado rama ");
+        System.out.println("2. git add . /agrupar archivos ");
+        System.out.println("3. git commit -m /guardar en local ");
+        System.out.println("4. git push origin main /subir al reositorio ");
+        System.out.println("5. git clone /clonar ");
+        System.out.println("6. git branch /ramas creadas");
+        System.out.println("7. git fetch -v 7ramas remotas");
+        System.out.println("8. git switch -c /crear rama y posicionarnos  ");
         }
     }
